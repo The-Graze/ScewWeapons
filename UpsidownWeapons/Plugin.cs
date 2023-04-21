a52839390fd428318706e968f43ce03e5b93fab6
Graze <graze.the.sergal@gmail.com>
2023-04-21 11:05:11 +0100

    Add project files.

---
+++ UpsidownWeapons/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace UpsidownWeapons
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Update()
+        {
+            if (GunControl.Instance.currentWeapon.GetComponent<Flipper>() == null)
+            {
+                GunControl.Instance.currentWeapon.AddComponent<Flipper>();
+            }
+        }
+    }
+    class Flipper : MonoBehaviour 
+    {
+        void Awake()
+        {
+            OnEnable();
+        }
+        void Start()
+        {
+            OnEnable();
+        }
+        void OnEnable()
+        {
+            var rand = new Random();
+            Single rnd = rand.Next();
+            Single rnd1 = rand.Next();
+            Single rnd2 = rand.Next();
+            transform.rotation = Quaternion.Euler(rnd, rnd1, rnd2);
+        }
+    }
+}
---
+++ UpsidownWeapons/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UpsidownWeapons
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.upsidownweapons";
+        public const string Name = "UpsidownWeapons";
+        public const string Version = "1.0.0";
+    }
+}
